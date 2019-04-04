76c63ce2b34c94bbed809b86d179c4eedf87f482
zyh66615 <904703889@qq.com>
2019-04-04 11:06:27 +0800

    NO.5最长回文子串

--- Leetcode-5.cpp
+++ Leetcode-5.cpp
@@ -1,23 +1,35 @@
+
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        if(numRows==0)
-            return NULL;
-        if(numRows==1)
-            return s;
-        vector<string>rows(min(numRows,int(s.size())));
-        int cur=0;
-        bool flag=false;
-        for(auto c:s)
-        {
-            rows[cur]+=c;
-            if(cur==0||cur==numRows-1)
-                flag=!flag;
-            cur+=flag?1:-1;
+    string longestPalindrome(string s) {
+        int len = s.length();
+        int **dp = new int*[len+1];
+        for(int i = 0; i < len+1; i++)
+            dp[i] = new int[len+1];
+        for(int i = 0; i < len+1; i++){
+            for(int j = 0; j < len+1; j++){
+                dp[i][j] = 0;
+            }
         }
-        string res;
-        for(auto i:rows)
-            res+=i;
-        return res;
+        for(int j = 0; j < len; j++){
+            dp[j][j] = 1;
+            dp[j+1][j] = 1;//这里也就是说的要手动初始话的这种情况
+        }
+        int l = 0, r = 0;
+        for(int i = 1; i < len; i++){
+            for(int j = 0; j+i < len; j++){
+                if(s[j] == s[i+j] && dp[j+1][i+j-1]){//为了保证j+1 <= i+j-1, i >= 2那么将i也就是长度为1的手动初始化
+                    l = j;
+                    r = j + i;
+                    dp[l][r] = 1;
+                }
+            }
+        }
+        string str = "";
+        for(int i = l; i <= r; i++)
+            str += s[i];
+        delete dp;
+        return str;
+        
     }
 };
