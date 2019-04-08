28b4c2b5ea0ece2f3c84cdb9b0deb22f747e6d80
zyh66615 <904703889@qq.com>
2019-04-08 21:06:30 +0800

    NO.13 罗马数字转整数

---
+++ Leetcode-13.cpp
@@ -0,0 +1,25 @@
+static const auto _ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+static const auto _ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        unordered_map<char, int> m{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        for (int i = 0; i < s.size(); ++i) {
+            int val = m[s[i]];
+            if (i == s.size() - 1 || m[s[i+1]] <= m[s[i]]) res += val;
+            else res -= val;
+        }
+        return res;
+    }
+};
