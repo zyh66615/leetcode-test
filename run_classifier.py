3743c2d8c8a5b8e614e322239a61d61493bbb12d
zyh66615 <904703889@qq.com>
2019-06-18 11:13:46 +0800

    bert

--- run_classifier.py
+++ run_classifier.py
@@ -1015,4 +1015,4 @@ if __name__ == "__main__":
   flags.mark_flag_as_required("vocab_file")
   flags.mark_flag_as_required("bert_config_file")
   flags.mark_flag_as_required("output_dir")
-  tf.app.run()
+  tf.compat.v1.app.run()
